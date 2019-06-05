ac74c87b7efd2c4622450b09460900482cba8378
fei.wang20 <fei.wang20@geely.com>
2019-06-05 17:10:26 +0800

    修改logutils

--- base/src/main/java/com/wangfeixixi/base/BaseApp.java
+++ base/src/main/java/com/wangfeixixi/base/BaseApp.java
@@ -3,6 +3,7 @@ package com.wangfeixixi.base;
 import android.app.Application;
 
 import com.wangfeixixi.base.debug.CrashHandler;
+import com.wangfeixixi.base.debug.LogUtils;
 import com.wangfeixixi.base.utils.VersionUtils;
 import com.wangfeixixi.logxixi.AndroidLogAdapter;
 import com.wangfeixixi.logxixi.Logger;
@@ -17,6 +18,8 @@ public class BaseApp extends Application {
         if (VersionUtils.isApkInDebug()) {
             CrashHandler.getInstance().init();
             Logger.addLogAdapter(new AndroidLogAdapter());
+
+            LogUtils.tag("wangfeixixi");
         }
     }
 
--- base/src/main/java/com/wangfeixixi/base/debug/LogUtils.java
+++ base/src/main/java/com/wangfeixixi/base/debug/LogUtils.java
@@ -3,13 +3,12 @@ package com.wangfeixixi.base.debug;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 
+import com.wangfeixixi.base.utils.VersionUtils;
 import com.wangfeixixi.logxixi.Logger;
 import com.wangfeixixi.logxixi.Printer;
 
 
 public class LogUtils {
-    public static String TAG = "wangfeiTest";
-
 
     /**
      * Given tag will be used as tag only once for this method call regardless of the tag that's been
@@ -17,6 +16,7 @@ public class LogUtils {
      * be used for the subsequent log calls
      */
     public static Printer tag(@Nullable String tag) {
+        if (VersionUtils.isApkInDebug()) return null;
         return Logger.t(tag);
     }
 
@@ -24,34 +24,47 @@ public class LogUtils {
      * General log function that accepts all configurations as parameter
      */
     public static void log(int priority, @Nullable String tag, @Nullable String message, @Nullable Throwable throwable) {
+        if (VersionUtils.isApkInDebug()) return;
         Logger.log(priority, tag, message, throwable);
     }
 
     public static void d(@NonNull String message, @Nullable Object... args) {
+        if (VersionUtils.isApkInDebug()) return;
         Logger.d(message, args);
     }
 
     public static void d(@Nullable Object object) {
+        if (VersionUtils.isApkInDebug()) return;
         Logger.d(object);
     }
 
+    public static void dJsonString(@Nullable Object object) {
+        if (VersionUtils.isApkInDebug()) return;
+        Logger.dJsonString(object);
+    }
+
     public static void e(@NonNull String message, @Nullable Object... args) {
+        if (VersionUtils.isApkInDebug()) return;
         Logger.e(null, message, args);
     }
 
     public static void e(@Nullable Throwable throwable, @NonNull String message, @Nullable Object... args) {
+        if (VersionUtils.isApkInDebug()) return;
         Logger.e(throwable, message, args);
     }
 
     public static void i(@NonNull String message, @Nullable Object... args) {
+        if (VersionUtils.isApkInDebug()) return;
         Logger.i(message, args);
     }
 
     public static void v(@NonNull String message, @Nullable Object... args) {
+        if (VersionUtils.isApkInDebug()) return;
         Logger.v(message, args);
     }
 
     public static void w(@NonNull String message, @Nullable Object... args) {
+        if (VersionUtils.isApkInDebug()) return;
         Logger.w(message, args);
     }
 
@@ -60,6 +73,7 @@ public class LogUtils {
      * ie: Unexpected errors etc
      */
     public static void wtf(@NonNull String message, @Nullable Object... args) {
+        if (VersionUtils.isApkInDebug()) return;
         Logger.wtf(message, args);
     }
 
@@ -67,6 +81,7 @@ public class LogUtils {
      * Formats the given json content and print it
      */
     public static void json(@Nullable String json) {
+        if (VersionUtils.isApkInDebug()) return;
         Logger.json(json);
     }
 
@@ -74,6 +89,7 @@ public class LogUtils {
      * Formats the given xml content and print it
      */
     public static void xml(@Nullable String xml) {
+        if (VersionUtils.isApkInDebug()) return;
         Logger.xml(xml);
     }
 }
