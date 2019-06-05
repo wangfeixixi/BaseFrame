dcf3bb67e7c7041d4000ee930239615fb4cff20b
fei.wang20 <fei.wang20@geely.com>
2019-06-05 17:21:53 +0800

    修改logutils

--- base/src/main/java/com/wangfeixixi/base/debug/LogUtils.java
+++ base/src/main/java/com/wangfeixixi/base/debug/LogUtils.java
@@ -16,56 +16,57 @@ public class LogUtils {
      * be used for the subsequent log calls
      */
     public static Printer tag(@Nullable String tag) {
-        if (VersionUtils.isApkInDebug()) return null;
-        return Logger.t(tag);
+        if (VersionUtils.isApkInDebug())
+            return Logger.t(tag);
+        else return null;
     }
 
     /**
      * General log function that accepts all configurations as parameter
      */
     public static void log(int priority, @Nullable String tag, @Nullable String message, @Nullable Throwable throwable) {
-        if (VersionUtils.isApkInDebug()) return;
-        Logger.log(priority, tag, message, throwable);
+        if (VersionUtils.isApkInDebug())
+            Logger.log(priority, tag, message, throwable);
     }
 
     public static void d(@NonNull String message, @Nullable Object... args) {
-        if (VersionUtils.isApkInDebug()) return;
-        Logger.d(message, args);
+        if (VersionUtils.isApkInDebug())
+            Logger.d(message, args);
     }
 
     public static void d(@Nullable Object object) {
-        if (VersionUtils.isApkInDebug()) return;
-        Logger.d(object);
+        if (VersionUtils.isApkInDebug())
+            Logger.d(object);
     }
 
     public static void dJsonString(@Nullable Object object) {
-        if (VersionUtils.isApkInDebug()) return;
-        Logger.dJsonString(object);
+        if (VersionUtils.isApkInDebug())
+            Logger.dJsonString(object);
     }
 
     public static void e(@NonNull String message, @Nullable Object... args) {
-        if (VersionUtils.isApkInDebug()) return;
-        Logger.e(null, message, args);
+        if (VersionUtils.isApkInDebug())
+            Logger.e(null, message, args);
     }
 
     public static void e(@Nullable Throwable throwable, @NonNull String message, @Nullable Object... args) {
-        if (VersionUtils.isApkInDebug()) return;
-        Logger.e(throwable, message, args);
+        if (VersionUtils.isApkInDebug())
+            Logger.e(throwable, message, args);
     }
 
     public static void i(@NonNull String message, @Nullable Object... args) {
-        if (VersionUtils.isApkInDebug()) return;
-        Logger.i(message, args);
+        if (VersionUtils.isApkInDebug())
+            Logger.i(message, args);
     }
 
     public static void v(@NonNull String message, @Nullable Object... args) {
-        if (VersionUtils.isApkInDebug()) return;
-        Logger.v(message, args);
+        if (VersionUtils.isApkInDebug())
+            Logger.v(message, args);
     }
 
     public static void w(@NonNull String message, @Nullable Object... args) {
-        if (VersionUtils.isApkInDebug()) return;
-        Logger.w(message, args);
+        if (VersionUtils.isApkInDebug())
+            Logger.w(message, args);
     }
 
     /**
@@ -73,23 +74,23 @@ public class LogUtils {
      * ie: Unexpected errors etc
      */
     public static void wtf(@NonNull String message, @Nullable Object... args) {
-        if (VersionUtils.isApkInDebug()) return;
-        Logger.wtf(message, args);
+        if (VersionUtils.isApkInDebug())
+            Logger.wtf(message, args);
     }
 
     /**
      * Formats the given json content and print it
      */
     public static void json(@Nullable String json) {
-        if (VersionUtils.isApkInDebug()) return;
-        Logger.json(json);
+        if (VersionUtils.isApkInDebug())
+            Logger.json(json);
     }
 
     /**
      * Formats the given xml content and print it
      */
     public static void xml(@Nullable String xml) {
-        if (VersionUtils.isApkInDebug()) return;
-        Logger.xml(xml);
+        if (VersionUtils.isApkInDebug())
+            Logger.xml(xml);
     }
 }
